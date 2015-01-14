71bb4fa29de822c5696df1536c6ecaba42743f44
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-01-14 15:21:58 +0000

    Unforce simdization in airfoil's kernels

--- demos/sparsetiling/airfoil/update.h
+++ demos/sparsetiling/airfoil/update.h
@@ -1,14 +1,18 @@
 inline void update(double *qold, double *q, double *res, double *adt, double *rms)
 {
-  double del, adti;
+  double del[4];
+  double adti;
 
   adti = 1.0f/(*adt);
 
   #pragma simd
-  for (int n=0; n<4; n++) {
-    del    = adti*res[n];
-    q[n]   = qold[n] - del;
+  for (int n = 0; n < 4; n++) {
+    del[n] = adti*res[n];
+    q[n] = qold[n] - del[n];
     res[n] = 0.0f;
-    *rms  += del*del;
+  }
+
+  for (int n = 0; n < 4; n++) {
+    *rms += del[n]*del[n];
   }
 }
