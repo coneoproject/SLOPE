ad3bfc15347e7c6af224f4aac2f228cf94f91c14
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-01-30 14:10:21 +0000

    python interface: Change support for sets

--- python/slope_python.py
+++ python/slope_python.py
@@ -34,7 +34,7 @@ typedef struct {
   char* name;
   int size;
 } slope_set;
-    """
+"""
 
     set_def = "set_t* set_%s = set(sets[%d].name, sets[%d].size);"
 
@@ -44,11 +44,12 @@ typedef struct {
 #include "inspector.h"
 
 /****************************/
-// Inspector's ctypes-compatible data structures
+// Inspector's ctypes-compatible data structures and functions
 %(set_decl)s
+extern "C" void inspector(slope_set sets[%(n_sets)d]);
 /****************************/
 
-void inspector(slope_set* sets) {
+void inspector(slope_set sets[%(n_sets)d]) {
 
   %(set_defs)s
 
@@ -58,18 +59,29 @@ void inspector(slope_set* sets) {
 """
 
     def __init__(self):
-        self._sets = []
-        self._dats = []
-        self._maps = []
-        self._loops = []
+        self._sets, self._dats, self._maps, self._loops = [None]*4
 
     def get_arg_types(self):
-        return [ctypes.POINTER(Set)]
+        """This method must be called only after the Inspector has fully been
+        defined. Here ``fully defined`` means that calls to ``add_sets``,
+        ``add_dats``, ``add_maps``, and ``add_loops`` must have performed,
+        which is a requirement to return the correct data types. In such an
+        event, a ``SlopeError`` exception is raised."""
+        try:
+            return [self._sets._type_*len(self._sets)]
+        except TypeError:
+            raise SlopeError("Cannot return arguments types prior to initialization")
 
-    def add_set(self, name, size):
-        new_set = Set(name, size)
-        self._sets.append(new_set)
-        return ctypes.byref(new_set)
+    def add_sets(self, sets):
+        """Add ``sets`` to this Inspector
+
+        :param sets: iterator of 2-tuple, in which the first entry is the name of
+                     the set (a string), while the second entry is the size of the
+                     iteration set
+        """
+        sets = (Set*len(sets))(*[Set(name, size) for name, size in sets])
+        self._sets = sets
+        return sets
 
     def generate_code(self):
         # Sets code
@@ -77,8 +89,17 @@ void inspector(slope_set* sets) {
 
         return Inspector.code % {
             'set_decl': Inspector.struct_set,
-            'set_defs': "\n  ".join(set_defs)
+            'set_defs': "\n  ".join(set_defs),
+            'n_sets': len(self._sets)
         }
+
+
+class SlopeError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
 # Utility functions for the caller
@@ -87,6 +108,7 @@ def get_compile_opts():
     """Return a list of options that are expected to be used when compiling the
     inspector/executor code"""
     return ["-std=c++11"]
+
 
 def get_lib_name():
     """Return default name of the shared object resulting from compiling SLOPE"""
