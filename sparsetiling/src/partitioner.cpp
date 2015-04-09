3abf7c7583569433b2b1b469981260dea84b2124
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-04-09 09:14:11 +0100

    mpi: Extend partitioner to include halo regions

--- sparsetiling/src/partitioner.cpp
+++ sparsetiling/src/partitioner.cpp
@@ -10,16 +10,19 @@ map_t* partition (loop_t* loop, int tileSize)
 {
   // aliases
   int setSize = loop->set->size;
+  int setExecHalo = loop->set->execHalo;
+  int setNonExecHalo = loop->set->nonExecHalo;
 
-  int* indMap = new int[setSize];
+  int setTotalSize = setSize + setExecHalo + setNonExecHalo;
+  int* indMap = new int[setTotalSize];
 
+  // tile the local iteration space
   int nParts = setSize / tileSize;
   int reminderTileSize = setSize % tileSize;
   int nTiles = nParts + ((reminderTileSize > 0) ? 1 : 0);
-
   int tileID = -1;
-  int i;
-  for (i = 0; i < setSize - reminderTileSize; i++) {
+  int i = 0;
+  for (; i < setSize - reminderTileSize; i++) {
     tileID = (i % tileSize == 0) ? tileID + 1 : tileID;
     indMap[i] = tileID;
   }
@@ -28,5 +31,18 @@ map_t* partition (loop_t* loop, int tileSize)
     indMap[i] = tileID;
   }
 
-  return map ("i2t", set_cpy(loop->set), set("tiles", nTiles), indMap, setSize*1);
+  // tile the halo region. We create a single tile spanning /execHalo/ as well as
+  // a single tile spanning /nonExecHalo/
+  tileID++;
+  for (; i < setExecHalo; i++) {
+    indMap[i] = tileID;
+  }
+  nTiles = nTiles + ((setExecHalo > 0) ? 1 : 0);
+  tileID++;
+  for (; i < setNonExecHalo; i++) {
+    indMap[i] = tileID;
+  }
+  nTiles = nTiles + ((setExecHalo > 0) ? 1 : 0);
+
+  return map ("i2t", set_cpy(loop->set), set("tiles", nTiles), indMap, setTotalSize*1);
 }
